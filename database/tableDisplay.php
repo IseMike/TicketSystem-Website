c59764ad106272c635409ccd6aae66e20b0c3d99
Isaac Shepherd <shepherdi1998@gmail.com>
2021-12-14 14:04:48 -0500

    Started working with wamp64 for server and database management
    - Using wamp64 to make working with servers and databases easier
    - Using PhpMyAdmin for database management
    -  tableDisplay.php is mostly for testing and trying out php for accessing and displaying a database on a page.
    - ticketsystemdatabase.db contains the sql for all the tables and data that is currently within the database I'm using for tableDisplay.php

---
+++ database/tableDisplay.php
@@ -0,0 +1,58 @@
+<?php 
+	$username = "root";
+	$password = "";
+	$host = "localhost";
+
+	$connector = mysqli_connect($host, $username, $password, "ticket")
+		or die("Unable to connect to my sql.");
+
+	//$selected = mysqli_select_db("ticket", $connector)
+	//	or die("Unable to connect to ticket database.");
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<meta charset="utf-8">
+ 	<title>Employees</title>
+ 	<style>
+ 		table, tr, td {
+ 			border: 1px solid black;
+ 		}
+ 	</style>
+ 	<h1>
+ 		Employee Table
+ 	</h1>
+ </head>
+ <body>
+ 	<?php 
+ 		$query = mysqli_query($connector, "SELECT * FROM employees");
+ 		//printf("SELECT returned %d rows. \n", mysqli_num_rows($query));
+ 	 ?>
+ 	 <table>
+ 	 	<thead>
+ 	 		<tr>
+ 	 			<th>Employee ID</th>
+ 	 			<th>Employee Name</th>
+ 	 			<th>Admin?</th>
+ 	 			<th>Tech?</th>
+ 	 		</tr>
+ 	 	</thead>
+ 	 	<tbody>
+ 	 		<?php 
+ 	 		while ($row = mysqli_fetch_assoc($query)) {
+ 	 			echo 
+ 	 			"<tr>
+ 	 			<td>{$row['EmployeeID']}</td>
+ 	 			<td>{$row['EmployeeName']}</td>
+ 	 			<td>{$row['Admin']}</td>
+ 	 			<td>{$row['Tech']}</td>
+ 	 			</tr>
+ 	 			";
+ 	 		}
+ 	 		 ?>
+ 	 	</tbody>
+ 	 </table>
+ </body>
+ </html>
+ <?php mysqli_close($connector) ?>
