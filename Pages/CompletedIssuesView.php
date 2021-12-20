c48e0c8ca81d01a0835fd95bddeae1b5d52b8d86
Isaac Shepherd <shepherdi1998@gmail.com>
2021-12-20 12:44:15 -0500

    Created More Views
    Created the completed issues view for admins to see what issues have been completed.
    Created the issues view for techs and admins to see what issues are currently in the system.
    Created the employees view for admins to view employees.

---
+++ Pages/CompletedIssuesView.php
@@ -0,0 +1,62 @@
+<?php 
+	$username = "root";
+	$password = "";
+	$host = "localhost";
+
+	$connector = mysqli_connect($host, $username, $password, "ticket")
+		or die("Unable to connect to my sql.");
+
+	//$selected = mysqli_select_db("ticket", $connector)
+	//	or die("Unable to connect to ticket database.");
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<meta charset="utf-8">
+ 	<title>Completed Issues</title>
+ 	<style>
+ 		table, tr, td {
+ 			border: 1px solid black;
+ 		}
+ 	</style>
+ 	<h1>
+ 		Completed Issues Table
+ 	</h1>
+ </head>
+ <body>
+ 	<?php 
+ 		$query = mysqli_query($connector, "SELECT * FROM completedissues");
+ 		//printf("SELECT returned %d rows. \n", mysqli_num_rows($query));
+ 	 ?>
+ 	 <table>
+ 	 	<thead>
+ 	 		<tr>
+ 	 			<th>Name</th>
+ 	 			<th>Subject</th>
+ 	 			<th>Description</th>
+ 	 			<th>UrgencyLevel</th>
+ 	 			<th>AssignedTech</th>
+ 	 			<th>IssueID</th>
+ 	 		</tr>
+ 	 	</thead>
+ 	 	<tbody>
+ 	 		<?php 
+ 	 		while ($row = mysqli_fetch_assoc($query)) {
+ 	 			echo 
+ 	 			"<tr>
+ 	 			<td>{$row['Name']}</td>
+ 	 			<td>{$row['Subject']}</td>
+ 	 			<td>{$row['Description']}</td>
+ 	 			<td>{$row['UrgencyLevel']}</td>
+ 	 			<td>{$row['AssignedTech']}</td>
+ 	 			<td>{$row['IssueID']}</td>
+ 	 			</tr>
+ 	 			";
+ 	 		}
+ 	 		 ?>
+ 	 	</tbody>
+ 	 </table>
+ </body>
+ </html>
+ <?php mysqli_close($connector) ?>
